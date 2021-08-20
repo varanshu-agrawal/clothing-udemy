25d63ed65168914ae72cdfd88131c9a933e9695c
varanshu-agrawal <varanshu.agrawal@digitup.in>
2021-08-20 18:30:27 +0530

    connect server to client

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+import { response } from 'express';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+    axios({
+      url: 'payment',
+      method: 'POST',
+      data: {
+        amount: priceForStripe,
+        token: token,
+
+      }
+    }).then((response) => {
+      alert('Payment Succesful!');
+    }).catch(error => {
+      console.log('Payment, error: ',JSON.parse(error));
+      alert('There was an issue with your payment. Please sure you use the provided credit card');
+    })
   };
 
   return (
