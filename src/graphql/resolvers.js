c806b48720084fdf0eac5c0f39b44eefd46254d9
varanshu-agrawal <varanshu.agrawal@digitup.in>
2021-08-27 16:31:07 +0530

    Change Cart items count

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,6 +1,6 @@
 import { gql } from 'apollo-boost';
 
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, getCartItemCount } from './cart.utils';
 
 export const typeDefs = gql`
     extend type Item{
@@ -18,6 +18,12 @@ const GET_CART_HIDDEN = gql`
         cartHidden @client
     }
 `
+
+const GET_ITEM_COUNT = gql`
+    {
+        itemCount @client
+    }
+`;
 
 const GET_CART_ITEMS = gql`
     {
@@ -39,13 +45,18 @@ export const resolvers = {
 
             return !cartHidden;
         },
-        
+
         addItemToCart: (_root, { item }, { cache }) => {
             const { cartItems } = cache.readQuery({
                 query: GET_CART_ITEMS
             });
 
             const newCartItems = addItemToCart(cartItems, item);
+
+            cache.writeQuery({
+                query: GET_ITEM_COUNT,
+                data: { itemCount: getCartItemCount(newCartItems) }
+            })
 
             cache.writeQuery({
                 query: GET_CART_ITEMS,
